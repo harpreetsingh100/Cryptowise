665af3094db29d8289255dcea62310434db16c2f
Harpreet <harpreetsingh25102001@gmail.com>
2024-06-06 22:55:03 -0700

    added rtk query and modified the store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,17 @@
+
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./features/api";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
   });
 };
 
-// Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>
-export type AppDispatch = AppStore["dispatch"]
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
